0b7954695f2361ce13b273e8349f855cd0b1b6f9
Rotem Patrakeev <rotem8y@gmail.com>
2023-05-19 16:44:42 +0300

    add custome domain

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,9 +9,9 @@ const Navbar = ({ email }) => {
 
     return (
         <div className="navbar header-item">
-            <Link className="nav-link" to="/rotem-patrakeev-portfolio" onClick={() => dispatch(toggalMenu())}>Home</Link>
-            <Link className="nav-link" to="/rotem-patrakeev-portfolio/About" onClick={() => dispatch(toggalMenu())}>About</Link>
-            <Link className="nav-link" to="/rotem-patrakeev-portfolio/Experience" onClick={() => dispatch(toggalMenu())}>Experience</Link>
+            <Link className="nav-link" to="/" onClick={() => dispatch(toggalMenu())}>Home</Link>
+            <Link className="nav-link" to="/About" onClick={() => dispatch(toggalMenu())}>About</Link>
+            <Link className="nav-link" to="/Experience" onClick={() => dispatch(toggalMenu())}>Experience</Link>
             {/* <Link className="nav-link" to="/Portfolio" onClick={() => dispatch(toggalMenu())}>Portfolio</Link> */}
             {email && <a className="nav-link buttons" href={`mailto:${email}`} >Contact Me</a>}
         </div>
